3c10839a475c94e8f2d7ee81bb51659dd4761a25
Shubashini m <shubashinimuthukumaran@gmail.com>
2024-10-04 00:31:26 +0530

    library_react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './HomePage';
 import './App.css';
+import BookDashboard from './BookDashboard';
 
-function App() {
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          {!isLoggedIn ? (
+            <Route path="/" element={<HomePage onLogin={handleLogin} />} />
+          ) : (
+            <Route path="/" element={<BookDashboard open={true} onClose={() => setIsLoggedIn(false)} />} />
+          )}
+        </Routes>
+      </div>
+    </Router>
   );
-}
-
+};
 export default App;
